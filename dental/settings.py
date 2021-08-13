26393bd4f043ba701dd3a86c4ced001eeeb2acc6
Saifriyami <saifriyami@gmail.com>
2021-08-13 08:53:34 +0400

    Changing in DB settings

--- dental/settings.py
+++ dental/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'dental.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        "DBNAME": "postgre",
+        "USER": "SAIF",
+        "PASSWORD": "SAIF1234",
+
+
+
     }
 }
 
